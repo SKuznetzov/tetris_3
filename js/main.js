269e7086124cd731e6d14f82a1e5647a14ac5116
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-26 13:35:58 +0300

    add array squares

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
-  let squares = document.querySelectorAll('.grid div')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
   const width = 10
 
 
