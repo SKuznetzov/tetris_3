dd021db19b336ecfcd8dfbee8c535fc99861ba6f
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-26 14:08:08 +0300

    add lTetromino

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
-
+   
+  const lTetromino = [
+      [1,width+1,width+2+1, 2],
+      [width,width+1, width+2,width+2+2],
+      [1, width+1, width+2+1, width+2],
+      [width, width+2, width+2+1, width+2+2]
+  ]
 
 
 
