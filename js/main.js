17a74363e0ed62b5e2b29343a8cd34d88e0128ec
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-26 13:31:51 +0300

    add grid ,squares and width

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = document.querySelectorAll('.grid div')
+  const width = 10
+
+
+
+
+
 
 })
