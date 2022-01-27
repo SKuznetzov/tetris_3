d2a69ca7641c3974cf106cafd322e481df79f279
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-27 15:16:38 +0300

    change * and +

--- js/main.js
+++ js/main.js
@@ -6,24 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
    
   const lTetromino = [
-      [1,width+1,width+2+1, 2],
+      [1,width+1,width*2+1, 2],
       [width,width+1, width+2,width+2+2],
-      [1, width+1, width+2+1, width+2],
-      [width, width+2, width+2+1, width+2+2]
+      [1, width+1, width*2+1, width*2],
+      [width, width*2, width*2+1, width*2+2]
   ]
 
   const zTetromino = [
-    [0,width,width+1, width+2+1],
-    [width+1,width+2, width+2,width+2+1],
-    [0, width, width+1, width+2+1],
-    [width+1, width+2, width+2, width+2+1]
+    [0,width,width+1, width*2+1],
+    [width+1,width+2, width+2,width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
   ]
 
   const tTetromino = [
     [1,width,width+1, width+2],
-    [1,width+1, width+2,width+2+1],
-    [width, width+1, width+2, width+2+1],
-    [1,width, width+1, width+2+1]
+    [1,width+1, width+2,width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1,width, width+1, width*2+1]
   ]
 
   const oTetromino = [
@@ -34,9 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const iTetromino = [
-    [1,width+1,width+2+1, width+3+1],
+    [1,width+1,width*2+1, width*3+1],
     [width,width+1, width+2,width+3],
-    [1, width+1, width+2+1, width+3+1],
+    [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3]
   ]
 
