abe12a103e269c0cbb487add435c0a51419990a3
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-27 14:23:01 +0300

    add arrey

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width+1, width+2, width+3]
   ]
 
-
+  const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
 
 
 
