1c8800499849110c676633023e7e274feafe18ad
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-28 15:14:46 +0300

    function freeze

--- js/main.js
+++ js/main.js
@@ -66,7 +66,20 @@ document.addEventListener('DOMContentLoaded', () => {
     undraw()
     currentPosition += width
     draw()
+    freeze()
   }
+
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+      random = math.floor(Math.random()* theTetrominoes.length)
+      current = theTetrominoes[random][currentPosition]
+      currentPosition = 4
+      draw()
+   }
+ }
+
 }
 
 
