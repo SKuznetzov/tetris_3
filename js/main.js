e29c4aca2cc90104718da54a88c00c88c33764e9
SKuznetzov <s-kuznecov100@rambler.ru>
2022-01-29 21:31:24 +0300

    add class token

--- js/main.js
+++ js/main.js
@@ -70,10 +70,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function freeze() {
-    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('token'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('token'))
 
-      random = math.floor(Math.random()* theTetrominoes.length)
+      random = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
